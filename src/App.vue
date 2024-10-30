b4ead8bfdb203ff9b4a14dd1bc5b5d1a97960e46
Bruno-Alvez <bruno.bsantos75@gmail.com>
2024-10-30 05:44:48 -0300

    subindo o projeto

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,34 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// Javascript 
+import Header from './components/Header.vue'
+import Calc from './components/Calc.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <!-- Html -->
+  <div class="container">
+    <Header></Header>
+    <Calc></Calc>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+/* Css */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-.logo {
+.container {
   display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  width: 640px;
+  max-width: 100%;
+  height: 800px;
+  background-color: #ccc;
+  margin: 0 auto;
+  border-radius: 15px;
+  padding: 20px;
+  margin-top: 25px;
 }
 </style>
